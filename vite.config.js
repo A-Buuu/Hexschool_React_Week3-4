d2b3fa9787cb6dddf71c26fd5959c67c16883958
abuchiang <abuuu.chiang@gmail.com>
2025-06-10 21:13:59 +0800

    github page setting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/Hexschool_React_Week3-4/" : "/",
   plugins: [react()],
-})
+});
