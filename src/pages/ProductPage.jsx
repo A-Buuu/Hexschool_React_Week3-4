fd7d8b3d0d626953b96099c00d36f5ab7e374c93
abuchiang <abuuu.chiang@gmail.com>
2025-07-03 21:25:20 +0800

    拆分分頁元件

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 // 只解構出需要用的到功能: Modal
 import { Modal } from "bootstrap";
 
+import Pagination from "../components/Pagination";
+
 const API_BASE = import.meta.env.VITE_BASE_URL;
 const API_PATH = import.meta.env.VITE_API_PATH;
 
@@ -318,53 +320,10 @@ function ProductPage() {
               )}
             </tbody>
           </table>
-          <div className="d-flex justify-content-center">
-            <nav>
-              <ul className="pagination">
-                <li className={`page-item ${!pageInfo.has_pre && "disabled"}`}>
-                  <a
-                    className="page-link"
-                    href="#"
-                    onClick={() => handlePageChange(pageInfo.current_page - 1)}
-                  >
-                    上一頁
-                  </a>
-                </li>
-
-                {
-                  // 產生相對應長度的陣列
-                  Array.from({ length: pageInfo.total_pages }).map(
-                    (_, index) => (
-                      <li
-                        className={`page-item ${
-                          pageInfo.current_page === index + 1 && "active"
-                        }`}
-                        key={index}
-                      >
-                        <a
-                          className="page-link"
-                          href="#"
-                          onClick={() => handlePageChange(index + 1)}
-                        >
-                          {index + 1}
-                        </a>
-                      </li>
-                    )
-                  )
-                }
-
-                <li className={`page-item ${!pageInfo.has_next && "disabled"}`}>
-                  <a
-                    className="page-link"
-                    href="#"
-                    onClick={() => handlePageChange(pageInfo.current_page + 1)}
-                  >
-                    下一頁
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <Pagination
+            handlePageChange={handlePageChange}
+            pageInfo={pageInfo}
+          />
         </div>
       </div>
 
